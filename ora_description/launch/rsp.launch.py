31f61ad7c48ba0ba96f3a1da7ff58e20084d9839
ZacharyLain <zacharytlain@gmail.com>
2025-11-03 19:43:14 -0500

    Fixed basic model
    Basic model more accurate to base frame of the robot now.

--- ora_description/launch/rsp.launch.py
+++ ora_description/launch/rsp.launch.py
@@ -1,40 +1,50 @@
-import os
-
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration, Command
 from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
-
-import xacro
+from launch_ros.substitutions import FindPackageShare
+import os
 
 def generate_launch_description():
+    pkg_path = os.path.join(get_package_share_directory('ora_description'))
+    default_model_path = os.path.join(pkg_path, 'description', 'anteater.urdf.xacro')
+    default_rviz_config_path = os.path.join(pkg_path, 'rviz', 'config.rviz')
 
-  # Check if we are using sim time
-  use_sim_time = LaunchConfiguration('use_sim_time')
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+    )
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', default_model_path])}],
+        condition=UnlessCondition(LaunchConfiguration('gui'))
+    )
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        condition=IfCondition(LaunchConfiguration('gui'))
+    )
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rvizconfig')],
+    )
 
-  # Process the URDF file
-  pkg_path = os.path.join(get_package_share_directory('ora_description'))
-  xacro_file = os.path.join(pkg_path,'description','anteater.urdf.xacro')
-  robot_description_config = Command(['xacro ', xacro_file, ' sim_mode:=', use_sim_time])
-    
-  # Create a robot_state_publisher node
-  params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
-  
-  node_robot_state_publisher = Node(
-    package='robot_state_publisher',
-    executable='robot_state_publisher',
-    output='screen',
-    parameters=[params]
-  )
-
-  # Launch
-  return LaunchDescription([
-    DeclareLaunchArgument(
-      'use_sim_time',
-      default_value='false',
-      description='Use sim time if true'),
-
-    node_robot_state_publisher
-  ])
+    return LaunchDescription([
+        DeclareLaunchArgument(name='gui', default_value='True', description='Flag to enable joint_state_publisher_gui'),
+        DeclareLaunchArgument(name='model', default_value=default_model_path, description='Absolute path to robot model file'),
+        DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path, description='Absolute path to rviz config file'),
+        joint_state_publisher_node,
+        joint_state_publisher_gui_node,
+        robot_state_publisher_node,
+        rviz_node
+    ])
